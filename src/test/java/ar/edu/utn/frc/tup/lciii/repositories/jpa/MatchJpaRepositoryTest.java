f3fd188e8312f452bf72a8a0c820d32a5702ff0b
405132-Mansilla-Santiago <405132@tecnicatura.frc.utn.edu.ar>
2025-05-22 15:45:50 -0300

    .

--- src/test/java/ar/edu/utn/frc/tup/lciii/repositories/jpa/MatchJpaRepositoryTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lciii/repositories/jpa/MatchJpaRepositoryTest.java
@@ -1,19 +1,48 @@
 package ar.edu.utn.frc.tup.lciii.repositories.jpa;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import ar.edu.utn.frc.tup.lciii.entities.MatchEntity;
+import ar.edu.utn.frc.tup.lciii.entities.PlayerEntity;
+import ar.edu.utn.frc.tup.lciii.models.MatchStatus;
 
-import static org.junit.jupiter.api.Assertions.fail;
 
 @DataJpaTest
 class MatchJpaRepositoryTest {
 
-
     @Test
     void getAllByPlayerIdAndMatchStatusTest() {
-        // TODO: Completar el Test para validar que el metodo
+        // DONE: Completar el Test para validar que el metodo
         //  getAllByPlayerIdAndMatchStatus(Long playerId, MatchStatus matchStatus)
         //  de la clase MatchJpaRepository funciona.
-        fail();
+
+        //Arrange
+        PlayerEntity player = new PlayerEntity();
+        player.setId(3L);
+        MatchEntity matchEntity = new MatchEntity();
+        matchEntity.setPlayer(player);
+        matchEntity.setMatchStatus(MatchStatus.FINISH);
+        Optional<List<MatchEntity>> matches = Optional.of(List.of(matchEntity));
+
+        MatchJpaRepository matchJpaRepositoryMock = mock(MatchJpaRepository.class);
+        //matchJpaRepositoryMock.save(matchEntity);
+
+        when(matchJpaRepositoryMock.getAllByPlayerIdAndMatchStatus(3L, MatchStatus.FINISH))
+            .thenReturn(matches);
+
+        //Act
+        Optional<List<MatchEntity>> result = matchJpaRepositoryMock.getAllByPlayerIdAndMatchStatus(3L, MatchStatus.FINISH);
+
+        //Assert
+        assertEquals(matches, result);
+        assertEquals(3L, matches.get().get(0).getPlayer().getId());
+        assertEquals(MatchStatus.FINISH, matches.get().get(0).getMatchStatus());
     }
 }
