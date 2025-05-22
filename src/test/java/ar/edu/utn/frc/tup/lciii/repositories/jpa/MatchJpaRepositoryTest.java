e4f34dffcea89f0362a9d8b0205fa585c8bcdfc4
405132-Mansilla-Santiago <405132@tecnicatura.frc.utn.edu.ar>
2025-05-22 16:57:51 -0300

    dff

--- src/test/java/ar/edu/utn/frc/tup/lciii/repositories/jpa/MatchJpaRepositoryTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lciii/repositories/jpa/MatchJpaRepositoryTest.java
@@ -1,47 +1,39 @@
 package ar.edu.utn.frc.tup.lciii.repositories.jpa;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import ar.edu.utn.frc.tup.lciii.entities.MatchEntity;
-import ar.edu.utn.frc.tup.lciii.entities.PlayerEntity;
-import ar.edu.utn.frc.tup.lciii.models.MatchStatus;
+
 
 
 @DataJpaTest
 class MatchJpaRepositoryTest {
 
-    @Test
-    void getAllByPlayerIdAndMatchStatusTest() {
-        // DONE: Completar el Test para validar que el metodo
-        //  getAllByPlayerIdAndMatchStatus(Long playerId, MatchStatus matchStatus)
-        //  de la clase MatchJpaRepository funciona.
+    // @
+    // private MatchJpaRepository matchJpaRepository;
 
-        //Arrange
-        PlayerEntity player = new PlayerEntity();
-        player.setId(3L);
-        MatchEntity matchEntity = new MatchEntity();
-        matchEntity.setPlayer(player);
-        matchEntity.setMatchStatus(MatchStatus.FINISH);
-        Optional<List<MatchEntity>> matches = Optional.of(List.of(matchEntity));
+    // @Test
+    // void getAllByPlayerIdAndMatchStatusTest() {
+    //     // DONE: Completar el Test para validar que el metodo
+    //     //  getAllByPlayerIdAndMatchStatus(Long playerId, MatchStatus matchStatus)
+    //     //  de la clase MatchJpaRepository funciona.
 
-        MatchJpaRepository matchJpaRepositoryMock = mock(MatchJpaRepository.class);
-        //matchJpaRepositoryMock.save(matchEntity);
+    //     //Arrange
+    //     PlayerEntity player = new PlayerEntity();
+    //     player.setId(3L);
 
-        when(matchJpaRepositoryMock.getAllByPlayerIdAndMatchStatus(3L, MatchStatus.FINISH))
-            .thenReturn(matches);
+    //     MatchEntity matchEntity = new MatchEntity();
+    //     matchEntity.setPlayer(player);
+    //     matchEntity.setMatchStatus(MatchStatus.FINISH);
 
-        //Act
-        Optional<List<MatchEntity>> result = matchJpaRepositoryMock.getAllByPlayerIdAndMatchStatus(3L, MatchStatus.FINISH);
+    //     Optional<List<MatchEntity>> matches = Optional.of(List.of(matchEntity));
+    //     when(matchJpaRepository.getAllByPlayerIdAndMatchStatus(3L, MatchStatus.FINISH)).
 
-        //Assert
-        assertEquals(matches, result);
-        assertEquals(3L, matches.get().get(0).getPlayer().getId());
-        assertEquals(MatchStatus.FINISH, matches.get().get(0).getMatchStatus());
-    }
+
+       
+    //     //Act
+    //     Optional<List<MatchEntity>> result = matchJpaRepository.getAllByPlayerIdAndMatchStatus(3L, MatchStatus.FINISH);
+
+    //     //Assert
+    //     assertEquals(matches, result);
+    //     assertEquals(3L, matches.get().get(0).getPlayer().getId());
+    //     assertEquals(MatchStatus.FINISH, matches.get().get(0).getMatchStatus());
+    // }
 }
